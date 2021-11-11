dff73cff20f5ac9bdb7c79266a038897eca1a71b
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-11-11 21:14:26 +0300

    added transitions between states

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,12 @@ export const TICK_RATE = 3000
 export const ICONS = ['fish', 'poop', 'weather']
 export const RAIN_CHANCE = 0.7
 export const SCENES = ['day', 'rain']
+export const DAY_LENGTH = 10
+export const NIGHT_LENGTH = 3
+export const getNextHungerTime = (clock) => {
+  return Math.floor(Math.random() * 3) + 5 + clock
+}
+export const getNextDieTime = (clock) => {
+  return Math.floor(Math.random() * 2) + 3 + clock
+}
 
--- src/gameState.js
+++ src/gameState.js
@@ -1,19 +1,28 @@
 import { modFox, modScene } from './ui'
-import { RAIN_CHANCE } from './constants'
-import { SCENES } from './constants'
+import { RAIN_CHANCE, SCENES, NIGHT_LENGTH, DAY_LENGTH,getNextDieTime, getNextHungerTime } from './constants'
 
 const gameState = {
   current: "INIT",
   clock: 1,
   wakeTime: -1,
+  sleepTime: -1,
   scene: 0,
+  hungryTime: -1,
+  dieTime: -1,
   tick() {
     this.clock++
     console.log('clock', this.clock)
 
     if (this.wakeTime === this.clock) {
       this.wake()
+    } else if (this.clock === this.sleepTime) {
+      this.sleep()
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry()
+    } else if (this.clock === this.dieTime) {
+      this.die()
     }
+
     return this.clock
   },
   startGame() {
@@ -30,6 +39,8 @@ const gameState = {
     modFox('idling')
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1
     modScene(SCENES[this.scene])
+    this.sleepTime = this.clock + DAY_LENGTH
+    this.hungryTime = getNextHungerTime(this.clock)
   },
   handleUserAction(icon) {
     if (['SLEEP', 'FEEDING', 'CELEBRATIN', 'HATCHING'].includes(this.current)) {
@@ -57,10 +68,32 @@ const gameState = {
     console.log('change weather')
   },
   feed() {
-    console.log('feed')
+    if (this.current !== 'HUNGRY') {
+      return
+    }
+    this.current = 'FEEDING'
+    this.dieTime = -1
+    this.poopTime = getNextPoopTime(this.clock)
+    modFox('eating')
+    this.timeToCelebrate = this.clock + 2
   },
   cleanUpPoot() {
     console.log('clean poop')
+  },
+  sleep() {
+    this.current = 'SLEEP'
+    modFox('sleep')
+    modScene('night')
+    this.wakeTime = this.clock + NIGHT_LENGTH
+  },
+  getHungry() {
+    this.current = 'HUNGRY'
+    this.dieTime = getNextDieTime(this.clock)
+    this.hungryTime = -1
+    modFox('hungry')
+  },
+  die() {
+
   }
 }
 
